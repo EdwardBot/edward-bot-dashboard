6d1e7563109c34665d00a70a26626d17a72781ca
Bendimester23 <bendimester23@gmail.com>
2021-03-24 16:54:09 +0100

    totally not forgot to add things

---
+++ src/components/Drawer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="drawer" v-if="show" v-bind:class="{ 'drawer-open': show, 'drawer-closed': !show }">
+      <h1>cucc</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Drawer",
+  computed: {
+    show: function(): boolean {
+      return this.$store.state.showDrawer && this.$store.state.hasDrawer;
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+.drawer {
+  background-color: #36393f;
+  z-index: 30;
+  overflow: hidden;
+  height: 100%;
+  color: #fff;
+}
+
+.drawer-open {
+  width: 260px;
+  animation: opening 1s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+}
+
+.drawer-closed {
+  width: 0px;
+  animation: closing 1s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+}
+
+@keyframes closing {
+  0% {
+    width: 260px;
+  }
+
+  100% {
+    width: 0px;
+  }
+}
+
+@keyframes opening {
+  100% {
+    width: 260px;
+  }
+
+  0% {
+    width: 0px;
+  }
+}
+</style>
